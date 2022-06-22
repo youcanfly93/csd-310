b0099e9fc3f65ec3d7b680300772b84515545d70
youcanfly93 <64027483+youcanfly93@users.noreply.github.com>
2022-06-21 23:19:22 -0700

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -2,19 +2,14 @@
 #06-19-2022
 #Module 5.2 Assignment
 
+'''This file will connect to a MongoDB database and output the containing folders'''
+
 import pymongo
 import certifi
 
 conn_str = "mongodb+srv://admin:admin@cluster0.uj3965s.mongodb.net/pytech?retryWrites=true&w=majority"
 
 client = pymongo.MongoClient(conn_str, tlsCAFile=certifi.where())
-
-'''try:
-    print("SUCESS")
-    print(client.server_info())
-except Exception:
-    print("Unable to connect")    
-'''
 
 db = client.pytech
 
